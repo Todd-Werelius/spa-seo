5095b4b93af957729162015ca6dd6983f90f5333
Todd-Werelius <Todd.Werelius@bthere.me>
2014-05-06 23:28:49 -0700

    Renamed server.js to render.js

--- render.js
+++ render.js
@@ -2,10 +2,10 @@
 
 // This will load a default config, then merge in config json file, and finally use any command line
 // configuration options
-var cfg       = require('./lib/config' )( process.argv );
-var prerender = require('prerender');
 
-var plugins   = {     // Available Plugins
+var cfg       = require('./lib/config')(process.argv);
+var prerender = require('prerender');
+var plugins   = {
     basicAuth         : prerender.basicAuth(),
     whitelist         : prerender.whitelist(),
     blacklist         : prerender.blacklist(),
@@ -16,22 +16,21 @@ var plugins   = {     // Available Plugins
     s3HtmlCache       : prerender.s3HtmlCache()
 };
 
+
 // Override prerender prodess.env variables if present
 cfg.env.forEach(function(item){
     if ( item.set ) {
+        //console.log("Overriding process.env." + key + " : " + process.env[item.key] + " to " + item.value);
         process.env[item.key] = item.value;
     }
 });
-
-// Initialize the prerender phantomJS service using our options
-// gathered from the cfg module
-var server = prerender(cfg.options);
 
 // Turn on sny requested and available plugins
 Object.keys(cfg.plugins).forEach(function(key) {
     if (key in plugins) {
         if (cfg.plugins[key]) {
             plugins[key];
+            //console.log("Loaded prerender plugin : "+key);
         }
     } else {
         console.log("Prerender plugin " + key + " was requested but is not recognized");
@@ -39,6 +38,14 @@ Object.keys(cfg.plugins).forEach(function(key) {
 });
 
 // Finally start up the prerender server to handle rendering the seo pages
-server.start();
+// Initialize the prerender phantomJS service using our options
+// gathered from the cfg module
+prerender(cfg.options).start();
+//engine.start();
+
+
+
+
+
 
 
