4320dd33ab53d4ddc49526131970e1f3d2e3ee1a
Todd-Werelius <Todd.Werelius@bthere.me>
2014-05-16 12:53:26 -0700

    Refactored Commander Block

--- render.js
+++ render.js
@@ -2,7 +2,7 @@
 
 // Configure and run the rendering server, use node render --help to view command line options, or
 // change the cfg.json file
-var prerenderServer = require('./lib/config').getPrerenderServer();
+var prerenderServer = require('./lib/config').getPrerenderServer(undefined);
 
 prerenderServer.start();
 
